fe171a324ae7212fed01322b59c22a2f456a27f3
kevinolivier <kevin.olivier@outlook.com>
2015-12-18 17:40:08 -0800

    Add a fairly basic setup.py

---
+++ setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+ 
+ 
+"""setup.py: setuptools control."""
+ 
+ 
+import re
+from setuptools import setup
+ 
+ 
+version = re.search(
+    "^__version__\s*=\s*'(.*)'",
+    open('translate/translate.py').read(),
+    re.M
+    ).group(1)
+ 
+with open("README.md", "rb") as f:
+    long_descr = f.read().decode("utf-8")
+ 
+setup(
+    name = 'translate-term',
+    packages = ['translate'],
+    install_requires=[
+        'requests>=2.8.1', 
+        'beautifulsoup4>=4.4.1', 
+        'terminaltables>=2.1.0' 
+        ]
+    entry_points = {
+        'console_scripts': ['translate = translate.translate:main']
+        },
+    version = version,
+    description = 'Command line translator pulling from wordreference.com and coded in Python',
+    long_description = long_descr,
+    keywords='shell bash zsh terminal translate wordreference',
+    license='MIT',
+    author = 'Kevin Olivier',
+    author_email = 'kevin.olivier@outlook.com',
+    url = "http://gehrcke.de/2014/02/distributing-a-python-command-line-application",
+    )
+
